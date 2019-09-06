e4d7962a8bccb28e1165088fd3205686625935dc
ds_090_25 <bryan.detres@upr.edu>
2019-09-06 17:10:56 -0400

    d

---
+++ dsf19lab2-master/src/edu/uprm/cse/datastructures/list/List.java
@@ -0,0 +1,34 @@
+package edu.uprm.cse.datastructures.list;
+
+public interface List<E> extends Iterable<E> {
+	
+	public int size();
+	
+	public boolean isEmpty();
+	
+	public boolean isMember(E e);
+	
+	public int firstIndexOf(E e);
+	
+	public int lastIndexOf(E e);
+
+	public void add(E e);
+	
+	public void add(E e, int position);
+	
+	public E get(int position);
+	
+	public E remove(int position);
+	
+	public E replace(int position, E newElement);
+	
+	public void clear();
+	
+	public Object[] toArray();	
+	
+	// NEW METHODS FOR LAB
+	public List<E> reverse();
+	
+	public int replaceAll(E e, E f);
+
+}
